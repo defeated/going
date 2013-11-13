b88e022ccea32a0b6203de5f7334b5a4a5841e4f
eddie cianci <defeated2k4@gmail.com>
2013-11-12 21:03:11 -0500

    naive fuzzy string search

--- going.go
+++ going.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"github.com/defeated/going/matcher"
 	"github.com/defeated/going/reader"
+	"os"
 )
 
 func main() {
 	lines := reader.Read("data.json")
-	matches := matcher.Match("w", lines)
+	input := os.Args[1]
+	matches := matcher.Match(input, lines)
 	fmt.Println(matches)
 }
--- matcher/matcher.go
+++ matcher/matcher.go
@@ -1,9 +1,20 @@
 package matcher
 
-import "regexp"
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+func fuzzify(input string) string {
+	chars := strings.Split(input, "")
+	fuzzy := ".*" + strings.Join(chars, ".*")
+	fmt.Println(fuzzy)
+	return fuzzy
+}
 
 func Match(input string, list []string) []string {
-	pattern := regexp.MustCompile("(?i).*" + input + ".*")
+	pattern := regexp.MustCompile("(?i)" + fuzzify(input))
 	matches := []string{}
 
 	for _, dir := range list {
